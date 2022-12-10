2747f70a39571c7af9ae56aaf4d4006f537856ae
webmaster0313 <94087189+webmaster0313@users.noreply.github.com>
2022-12-10 01:37:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/about.js
@@ -0,0 +1,30 @@
+export default () => {
+  return (
+    <>
+      <div className="container mt-32 mx-auto w-full">
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <h2  className="text-2xl font-bold mb-3">Legal notice</h2>
+        <p>
+          This website is the online version of the gitbup repo <a href="https://github.com/Benjamin-Roger/2d-car-game" rel="nofollow">https://github.com/Benjamin-Roger/2d-car-game</a>
+        </p>
+        <p>
+          It does not use cookies besides the microphone usage, which requires
+          systematic consent to the user. The user's browser handles the consent
+          according to its own policty.
+        </p>
+        <p>This consent is not kept.</p>
+        <p>This website has no commercial purpose.</p>
+        <strong>Host contact</strong>
+        <p>Vercel Inc.</p>
+        <p>340 S Lemon Ave #4133</p>
+        <p>Walnut, CA 91789</p>
+        <p>privacy@vercel.com</p>
+
+        <h2  className="text-2xl font-bold mb-3">Credits</h2>
+        <a href="https://www.freepik.com/vectors/car">
+          Car vector created by pch.vector - www.freepik.com
+        </a>
+      </div>
+    </>
+  );
+};
